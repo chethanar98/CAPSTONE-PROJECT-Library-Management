89d46c4520a7eaaf9dd46c506d99ba931161faea
chethanar98 <48445454+chethanar98@users.noreply.github.com>
2019-04-09 06:57:35 +0530

    Add files via upload

--- library_manager.py
+++ library_manager.py
@@ -1,111 +1,129 @@
+
 book_library = {"To Kill A Mocking Bird":10, "The Great Gatsby":4, "The Kite Runner":2, "Eleven Minutes":1}
 list_of_books = {"1" : "To Kill A Mocking Bird", "2" : "The Great Gatsby", "3" : "The Kite Runner" , "4" : "Eleven Minutes"}
 
 class LibraryManager:
-
+	"""
+	General Class for Library Account
+	"""
 	def __init__(self,member_name,pin,member_no): 
+		"""
+		Constructor Function for LibraryManager Class
+		Executed by interpreter to create an instance of a class 
+
+		Attributes:
+		self.member_name --- name of the member
+		self.pin --- unique PIN given by the member
+		self.member_no --- Memebrship number issued by the library
+		self.borrowed_books ---- Transaction made by the member
+		"""
 		self.member_name=member_name
 		self.pin=pin
-		self.no_of_books = 1
 		self.member_no = member_no
 		self.member = True
 		self.borrowed_books = {}
 	
 	
 	def show_list_of_books(self):
+		"""
+		Displays the books available in the library.
+		"""
+
 		print("Hello, these are the books available to you today : ")
+
 		for key,values in book_library.items():
-			
-
 			print(key)
-		
-
-	def display_count_of_books(self):
-		choice=input("Enter the book name you want to know if its available or not: \n\n\
-			1. To Kill a Mocking Bird\n\
-			2. The Great Gatsby\n\
-			3. The Kite Runner\n\
-			4. Eleven Minutes \n")
-		book_name=list_of_books[choice]
-		print(book_name, " is available, ", book_library[book_name], "book(s) are available.")
-		
 	
 	
 	def borrow_book(self):
-		choice=input("Enter the book name you want to borrow from the list given below\n\n\
+		"""
+		Allows the member to borrow book(s)
+		params:
+		copies available in the library - copies required by the member
+		"""
+		choice = input("Enter the book name you want to borrow from the list given below\n\n\
 			1. To Kill a Mocking Bird\n\
 			2. The Great Gatsby\n\
 			3. The Kite Runner\n\
 			4. Eleven Minutes \n")
-		book_name=list_of_books[choice]
+		
+		book_name = list_of_books[choice]
 
-		copies=int(input("Enter the number of copies required : "))
-		if book_library[book_name]==0:
+		copies = int(input("Enter the number of copies required : "))
+		
+		if book_library[book_name] == 0:
 			print("Sorry, ", book_name , " is not available")
+		
 		elif book_name in book_library.keys():
-			book_library[book_name]-=1
-			print(book_library)
+			book_library[book_name] -= 1
 			
 			if copies <= 0:
 					print("No book borrowed")
 			else:
 				self.borrowed_books[book_name] = copies
 
-
 		else:
 			print("Invalid book name")
-
-		
-		
-		
+	
 
 	def return_book(self):
-		
+		"""
+		Allows the member to return book(s) that were borrowed earlier
+		params:
+		copies the member wants to return + copies available in the library
+		""" 
+
 		for key, value in self.borrowed_books.items():
 			print("Your list of borrowed books is ",key, value, sep=" - ")
 
 		book_name = input("Enter the book you want to return from the list given above: ")
+		
 		for key,value in book_library.items():
+		
 			if book_name == key:
 				book_library[book_name]=book_library[book_name]+1
-				# print(book_library[book_name])
 				break
 
-
-		
 		copy_count = int(input("How many copies of this book would you like to return?\n"))
 
-
-		if copy_count<=value:
-			self.borrowed_books[book_name]=value+1
-			if self.borrowed_books.values()==0:
+		if copy_count <= value:
+			self.borrowed_books[book_name] = value+1
+			if self.borrowed_books.values() == 0:
 				del self.borrowed_books[book_name]
 			print(self.borrowed_books)
+		
 		else:
 			print("Invalid book name")
 		print(book_library)
-
-
-
 
 # --------------------------------------------------------------------------------------
 
 # Child Class 1:
 
 class RegularMember(LibraryManager):
+	"""
+	A child class created to become a Regular Member of the library. A regular member can borrow one book at a time.
+	"""
+
 	def borrow_book(self):
-		book_name=input("Enter the book name you want to borrow from the list given below\n\n\
+		"""
+		Allows the regular memeber to borrow one book.
+		params:
+		copies - 1
+		"""
+		book_name = input("Enter the book name you want to borrow from the list given below\n\n\
 			1. To Kill a Mocking Bird\n\
 			2. The Great Gatsby\n\
 			3. The Kite Runner\n\
 			4. Eleven Minutes \n").title()
-		copies=int(input("Enter the number of copies required : "))
-		if copies <=1:
+		copies = int(input("Enter the number of copies required : "))
+		if copies <= 1:
 		
-			if book_library[book_name]==0:
+			if book_library[book_name] == 0:
 				print("Sorry, ", book_name , " is not available")
+
 			elif book_name in book_library.keys():
-				book_library[book_name]-=1
+				book_library[book_name] -= 1
 				print(book_library)
 				if copies <= 0:
 					print("No book borrowed")
@@ -116,33 +134,40 @@ class RegularMember(LibraryManager):
 					for key, value in self.borrowed_books.items():
 						print(key, value, sep=" - ")
 
-
 			else:
 				print("Invalid book name")
+
 		else:
 			print("Sorry, you can borrow only one book at a time.")
-
-		
-
 
 # --------------------------------------------------------------------------------------
 
 #  Child Class 2:
 
 class GoldMember(LibraryManager):
+	"""
+	A child class created to become a Gold Member of the lbrary. A gold member can borrow one or two book(s) at a time.
+	"""
+
 	def borrow_book(self):
-		book_name=input("Enter the book name you want to borrow from the list given below\n\n\
+		"""
+		Allows the gold memeber to borrow one or two books at a time.
+		params:
+		copies - copies required
+		"""
+		book_name = input("Enter the book name you want to borrow from the list given below\n\n\
 			1. To Kill a Mocking Bird\n\
 			2. The Great Gatsby\n\
 			3. The Kite Runner\n\
 			4. Eleven Minutes \n").title()
-		copies=int(input("Enter the number of copies required : "))
+		copies = int(input("Enter the number of copies required : "))
 		if copies <= 2:
 		
-			if book_library[book_name]==0:
+			if book_library[book_name] == 0:
 				print("Sorry, ", book_name , " is not available")
+
 			elif book_name in book_library.keys():
-				book_library[book_name]-=copies
+				book_library[book_name] -= copies
 				print(book_library)
 				if copies <= 0:
 					print("No book borrowed")
@@ -153,17 +178,14 @@ class GoldMember(LibraryManager):
 					for key, value in self.borrowed_books.items():
 						print(key, value, sep=" - ")
 
-
 			else:
 				print("Invalid book name")
+
 		else:
 			print("Sorry, you can borrow atmost 2 books at a time.")
 
 # -------------------------------------------------------------------------------------
 
-
-
-		
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,98 +1,152 @@
-from library_manager import LibraryManager,RegularMember, GoldMember
-print("Welcome to PEEK-A-BOOK LIBRARY")
-accounts_regular={}
-accounts_gold={}
-member_id_regular=1000
-member_id_gold=2000
+"""
+Library Management Applocation Script
 
+Authors:
+		Chethana Ramesh <chethanaramesh98@gmail.com>
+"""
 
+from library_manager import LibraryManager, RegularMember, GoldMember
+from tabulate import tabulate
 
+"""
+**Variables**
+	
+	accounts_regular: (dict) {member_id_regular: Library Manager Object} 
+			  holds all the regular memberships created during the session.
+	accounts_gold: (dict) {member_id_gold: Library Manager Object} 
+			  holds all the gold memberships created during the session.
+	member_id_regular: (int) - Set to 1000 by default, member_id_regular. allocation starts here. 
+				Incremented after every new membership creation
+	member_id_gold: (int) - Set to 2000 by default, member_id_gold. allocation starts here. 
+				Incremented after every new membership creation
+"""
+
+accounts_regular = {}
+accounts_gold = {}
+member_id_regular = 1000
+member_id_gold = 2000
+print(" -"*20, "\nWelcome to Cover-to-Cover Library!\n","- "*20)
+
+# Application loop
 while True:
-	choice = int(input("Hello, what do you want to do today?\n\n\
-		1. Create membership\n\
-		2. Show list of books\n\
-		3. Borrow books\n\
-		4. Return books\n"))
+	
+	print("\n\n")	
+	print("How can we help you today?\n\nFeel free to choose from the following options.\n")
 
+	# Print the options in a table
+	print(tabulate([(1, "Create Membership",), (2, "Show list of books",), (3, "Borrow books",), (4, "Return books",), (5, "Exit",)], tablefmt="fancy_grid"))
+	
+	# Taking the user's choice from options as (int)
+	choice = int(input("\nPlease enter an option: "))
 
-	if choice==1:
-		account_name=input("Enter your name : ")
-		pin=int(input("Enter a PIN : "))
-		choice=int(input("Enter the type of membership you want?\n 1.Regular \n 2.Gold \n"))
+	if choice == 1:
+		"""
+		If user choice is 1 (int) - Create membership
+		"""
+		# Get User Member Details
+		account_name = input("Enter your name : ")
+		pin = int(input("Enter a PIN : "))
+		choice = int(input("Enter the type of membership you want?\n 1.Regular\n 2.Gold\n"))
 
-		if choice==1:
-			accounts_regular[member_id_regular]=RegularMember(account_name,pin,member_id_regular)
+		if choice == 1:
+			"""
+			If user choice is 1(int) - Create Regular Membership
+			"""
+			accounts_regular[member_id_regular] = RegularMember(account_name,pin,member_id_regular)
 			print("Congratulations, you are now a REGULAR member and your membership ID is: ", member_id_regular)
-			member_id_regular +=1
-		elif choice==2:
-			accounts_gold[member_id_gold]=GoldMember(account_name,pin,member_id_gold)
+			member_id_regular += 1
+
+		elif choice == 2:
+			"""
+			If user choice is 2(int) - Create Gold Membership
+			"""
+			accounts_gold[member_id_gold] = GoldMember(account_name,pin,member_id_gold)
 			print("Congratulations, you are now a GOLD member and your membership ID is: ", member_id_gold)
-			member_id_gold +=1
+			member_id_gold += 1
 
-		
-		
-	elif choice==2:
-		member_id=eval(input("Enter your Member ID : "))
-		upin=int(input("Enter your PIN : "))
-		choice=int(input("1.Regular \n2.Gold\n"))
+	elif choice == 2:
+		"""
+		If user choice is 2(int) - Perfron show list of books operation
+		"""
+		# Get User Member details
+		member_id = eval(input("Enter your Member ID : "))
+		upin = int(input("Enter your PIN : "))
+		choice = int(input("1.Regular\n2.Gold\n"))
 
-		if choice==1:
-
-			# if upin == accounts_regular[member_id].pin:
-			# 	accounts_regular[member_id].show_list_of_books()
-
+		if choice == 1:
+			"""
+			If user choice is 1(int) - check regular membership details and perform show list of books operation
+			"""
 			for key,val in accounts_regular.items():
 				if upin == accounts_regular[key].pin:
-					accounts_regular[key].show_list_of_books()	
-					
-
+					accounts_regular[key].show_list_of_books()			
 				else:
 					print("does not exist")
 		
-		elif choice==2:
-
-			if upin==accounts_gold[member_id].pin:
+		elif choice == 2:
+			"""
+			If user choice is 2(int) - check gold membership details and perform show list of books operation
+			"""
+			if upin == accounts_gold[member_id].pin:
 				accounts_gold[member_id].show_list_of_books()
 			else:
 				print("does not exist")
 
-
-		
-	elif choice==3:
-		member_id=eval(input("Enter your Member ID : "))
-		upin=eval(input("Enter your PIN : "))
-		choice=int(input("1.Regular\n2.Gold\n"))
-		if choice==1:
+	elif choice == 3:
+		"""
+		If user choice is 3(int) - Perform Borrow operation
+		"""
+		# Get User Member details
+		member_id = eval(input("Enter your Member ID : "))
+		upin = eval(input("Enter your PIN : "))
+		choice = int(input("1.Regular\n2.Gold\n"))
+		if choice == 1:
+			"""
+			If user choice is 1(int) - check regular membership details and perform borrow operation
+			"""
 			for key,val in accounts_regular.items():
 				if upin == accounts_regular[key].pin:
 					accounts_regular[key].borrow_book()	
+			
 				else:
 					print("does not exist")
+
 		elif choice == 2:
-			if upin==accounts_gold[member_id].pin:
+			"""
+			If user choice is 2(int) - check gold membership details and perform borrow operation
+			"""
+			if upin == accounts_gold[member_id].pin:
 				accounts_gold[member_id].borrow_book()
 			else:
 				print("does not exist")
 
-		
-		
-	elif choice==4:
-		member_id=eval(input("Enter your Member ID : "))
-		upin=eval(input("Enter your PIN : "))
+	elif choice == 4:
+		"""
+		If user choice is 4(int) - Perform Return operation
+		"""
+		# Get User Member details
+		member_id = eval(input("Enter your Member ID : "))
+		upin = eval(input("Enter your PIN : "))
+		choice = int(input("1.Regular\n2.Gold\n"))
 
-		choice=int(input("1. Regular\n2.Gold\n"))
-		if choice==1:
+		if choice == 1:
+			"""
+			If user choice is 1(int) - check regular membership details and perform return operation
+			"""
 			for key,val in accounts_regular.items():
 				if upin == accounts_regular[key].pin:
 					accounts_regular[key].return_book()	
 				else:
 					print("does not exist")
+
 		elif choice == 2:
-			if upin==accounts_gold[member_id].pin:
+			"""
+			If user choice is 2(int) - check gold membership details and perform return operation
+			"""
+			if upin == accounts_gold[member_id].pin:
 				accounts_gold[member_id].return_book()
 			else:
 				print("does not exist")
-	
 
 	else:
 		break
